5cc67d563a676bc42749bce8c7169724de3bcde6
C1cada <41801294+C1cada@users.noreply.github.com>
2020-04-21 16:02:35 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,22 @@
-var numClicks = 0
+var data = 0
+var expected = 10
 
 function onClick(){
-	numClicks += 1
-	document.getElementById("number").innerHTML = "Number of clicks: " + numClicks;
+	data += 1
+	document.getElementById("currency").innerHTML = "Data: " + data;
+    if (data >= expected){
+        CheckImprovemnt()
+    }
 }
+
+function CheckImprovemnt(){
+    if (data >= 10 && expected == 10){
+        var HelloWorld = document.createElement("BUTTON");
+
+        HelloWorld.innerHTML = "Hello World <span class='tooltip'> Hi </span>";
+        HelloWorld.classList.add('button');
+        document.body.appendChild(HelloWorld);
+
+        expected = 50
+    }
+}   
